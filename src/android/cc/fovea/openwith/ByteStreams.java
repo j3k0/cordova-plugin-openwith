fc9018aaa0a8296bb04deac245d0ebe0b9d32246
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-10-04 15:09:40 +0300

    Fix linting warnings

--- src/android/cc/fovea/openwith/ByteStreams.java
+++ src/android/cc/fovea/openwith/ByteStreams.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
 
 /**
  * Convert an InputStream to a byte array.
