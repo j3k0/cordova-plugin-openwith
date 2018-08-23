99cda4b44bef5f1bacde8e0647ad908e18944fb0
Niklas Merz <nmerz@gedys-intraware.de>
2018-08-23 10:51:26 +0200

    Add prefix to variables to avoid conflicts with other plugins

--- hooks/iosAddTarget.js
+++ hooks/iosAddTarget.js
@@ -281,9 +281,9 @@ module.exports = function (context) {
     });
 
     //Add development team and provisioning profile
-    var PROVISIONING_PROFILE = getCordovaParameter(configXml, 'PROVISIONING_PROFILE');
-    var DEVELOPMENT_TEAM = getCordovaParameter(configXml, 'DEVELOPMENT_TEAM');
-    console.log('Adding team', DEVELOPMENT_TEAM);
+    var PROVISIONING_PROFILE = getCordovaParameter(configXml, 'SHAREEXT_PROVISIONING_PROFILE');
+    var DEVELOPMENT_TEAM = getCordovaParameter(configXml, 'SHAREEXT_DEVELOPMENT_TEAM');
+    console.log('Adding team', DEVELOPMENT_TEAM, 'and provisoning profile', PROVISIONING_PROFILE);
     if (PROVISIONING_PROFILE && DEVELOPMENT_TEAM) {
       var configurations = pbxProject.pbxXCBuildConfigurationSection();
       for (var key in configurations) {
